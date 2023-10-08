76ac7e2d7167e1667c28dc32375ddd82a94a3a5a
Chiryasova Ekaterina <kat.rok@mail.ru>
2023-10-08 15:36:58 +0300

    Добавлены task

---
+++ task03_день_недели_по_заданному_числу/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Напишите программу, которая будет выдавать 
+название дня недели по заданному номеру
+3 -> среда
+5 -> пятница
+*/
+
+System.Console.WriteLine("Введите число от 1 до 7: ");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+{
+    System.Console.WriteLine("Сегодня понедельник");
+}
+else if (day == 2)
+{
+    System.Console.WriteLine("Сегодня вторник");
+}
+else if (day == 3)
+{
+    System.Console.WriteLine("Сегодня среда");
+}
+else if (day == 4)
+{
+    System.Console.WriteLine("Сегодня четверг");
+}
+else if (day == 5)
+
+{
+    System.Console.WriteLine("Сегодня пятница");
+}
+else if (day == 6)
+{
+    System.Console.WriteLine("Сегодня суббота");
+}
+else if (day == 7)
+{
+    System.Console.WriteLine("Сегодня воскресенье");
+}
+else
+System.Console.WriteLine("Ошибка");

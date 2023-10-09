c11a26dd8f7d4a5665088d86badfdea14c936443
Chiryasova Ekaterina <kat.rok@mail.ru>
2023-10-09 19:00:59 +0300

    Добавлено дз 02 04 06 08

---
+++ task02_большее_из_двух_чисел/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 
+Внутри класса Answer напишите метод CompareNumbers, который на вход принимает два числа и выводит,
+какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> Первое число 5 меньше чем второе число 7
+a = 2; b = 2 -> Введенные числа равны 2
+a = -3; b = -9 -> Первое число -3 больше чем второе число -9
+*/
+
+System.Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(System.Console.ReadLine());
+
+if (number1 > number2)
+{
+    System.Console.WriteLine(number1 + " больше чем " + number2);
+}
+else if (number1 < number2)
+{
+    System.Console.WriteLine(number2 + " больше чем " + number1);
+}
+else if (number1 == number2)
+{
+    System.Console.WriteLine(number1 + " равно " + number2);
+}
---
+++ task08_вывести_чётные_числа/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Внутри класса Answer напишите метод PrintEvenNumbers, которая на вход принимает число (number), а на выходе выводит все чётные числа 
+от 1 до number (включительно), разделеные знаком табуляции.
+*/
+
+System.Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("Число чётное: ");
+}
+else
+System.Console.WriteLine("Число нечётное: ");

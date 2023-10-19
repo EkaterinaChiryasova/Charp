54f2e5668b7e6b1880e94b0ea3696fdcb64e4243
Chiryasova Ekaterina <kat.rok@mail.ru>
2023-10-19 16:47:29 +0300

    Добавлены task 4 семинара и ДЗ

--- task23_кубы_от_1_до_N/Program.cs
+++ task23_кубы_от_1_до_N/Program.cs
@@ -1,7 +1,20 @@
 ﻿/*
 Напишите программу, которая принимает на вход число (N) 
 и выдаёт таблицу кубов чисел от 1 до N.
+
+N = 3 
+1
+8
+27
+
+N = 5
+1
+8
+27
+64
+125
 */
+
 
 System.Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(System.Console.ReadLine());
@@ -9,3 +22,4 @@ int n = Convert.ToInt32(System.Console.ReadLine());
 for (int i = 1; i <= n; i++)
     //System.Console.Write(Math.Pow(i, 3) + " ");
     System.Console.WriteLine($"{i} * {i} * {i} = {Math.Pow(i, 3)}");
+   
---
+++ task26_кол-во_цифр_в_числе/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу, которая принимает на вход число 
+и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(System.Console.ReadLine());
+
+/*int count = 0;
+
+for (int i = number; i > 0; i /= 10)
+{
+    count = count + 1;
+}
+*/
+
+int Result(int number)
+{
+    int count = 0;
+    for (int i = number; i > 0; i /= 10)
+    {
+        count = count + 1;
+    }
+    return(count)
+}
+int userCount = Result(userNumber);
+System.Console.WriteLine(userCount);

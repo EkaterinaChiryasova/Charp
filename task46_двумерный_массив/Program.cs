70bac43d34e6842eff252e91fffc6766fb7a4623
Chiryasova Ekaterina <kat.rok@mail.ru>
2023-11-03 16:36:35 +0300

    Добавлены task 7 семинара и ДЗ

---
+++ task46_двумерный_массив/Program.cs
@@ -0,0 +1,97 @@
+﻿/*
+Задайте двумерный массив размером m×n, 
+заполненный случайными целыми числами.
+*/ 
+
+// Обозначение индекса
+/*
+void Print2DArray(int[,] array)
+{
+System.Console.Write($"[ ]\t");
+int start = 0;
+for (int i = start; i < start + array.GetLength(1); i++)
+{
+System.Console.Write($"[{i}]\t");
+}
+System.Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+System.Console.Write($"["+ i +"]\t");
+for (int j = 0; j < array.GetLength(1); j++)
+{
+System.Console.Write(array[i, j] + "\t");
+}
+System.Console.WriteLine();
+}
+}
+*/
+
+Console.Clear();
+void Fill2DArray(int[,] arr)
+{
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+arr[i, j] = new Random().Next(-10, 100);
+}
+}
+}
+
+void Print2DArray(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+System.Console.Write($"{array[i, j]} ");
+}
+System.Console.WriteLine();
+}
+}
+int[,] array2D = new int[3, 4];
+Fill2DArray(array2D);
+Print2DArray(array2D);
+
+/* Console.Clear();
+
+int TakeEnteredNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+string[,] GetIndex2DArray(int row, int column)
+{
+    string[,] array = new string[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] =($"{i} + {j} = {i + j}");
+        }
+    }
+    return array;
+}
+
+
+void Print2DStringArray(string[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int userArrayRow = TakeEnteredNumber("Введите количестов строк:");
+int userArrayColumn = TakeEnteredNumber("Введите количестов столбцов:");
+string[,] user2DIndexArray = GetIndex2DArray(userArrayRow, userArrayColumn);
+
+Print2DStringArray(user2DIndexArray);
+*/
---
+++ task48_двумерный_массив_по_формуле/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задайте двумерный массив размера m на n, каждый элемент в массиве находится 
+по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3 
+1 2 3 4
+2 3 4 5
+*/
+
+        int m = 3;//задайте размер m
+        int n = 3;//задайте размер n
+        int[][] arr = new int[3][];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = new int[3];
+            for (int j = 0; j < arr[i].Length; j++)
+            {
+                arr[i][j] = i + j;
+            }
+
+        }
+        PrintArr(arr);
+    
+    static void PrintArr(int[][] arr)
+    {
+        foreach (var row in arr)
+        {
+            System.Console.WriteLine($"[{string.Join(" ", row)}]");
+        }
+    }
+
+

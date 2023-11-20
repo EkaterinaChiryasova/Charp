aeee532ad85c49eee84f537c369f8ad5a746eade
Chiryasova Ekaterina <kat.rok@mail.ru>
2023-11-20 17:35:12 +0300

    Добавлены task 8 и 9 семинара и ДЗ

--- task49_квадраты_четных_индексов_в_двумерном_массиве/Program.cs
+++ task49_квадраты_четных_индексов_в_двумерном_массиве/Program.cs
@@ -54,8 +54,8 @@ void Print2DArray(int[,] array)
 
 int userArrayRow = TakeEnteredNumber("Введите количестов строк:");
 int userArrayColumn = TakeEnteredNumber("Введите количестов столбцов:");
-int userArrayStart = TakeEnteredNumber("Введите начало диапозона:");
-int userArrayEnd = TakeEnteredNumber("Введите конец диапозона:");
+int userArrayStart = TakeEnteredNumber("Введите начало диапазона:");
+int userArrayEnd = TakeEnteredNumber("Введите конец диапазона:");
 System.Console.WriteLine();
 int[,] user2DArray = GetRandom2DArray(userArrayStart, userArrayEnd, userArrayRow, userArrayColumn);
 
@@ -65,4 +65,49 @@ int[,] userNewArray = ModifyEvenIndexSquare(user2DArray);
 Print2DArray(user2DArray);
 System.Console.WriteLine();
 Print2DArray(userNewArray);
+
+
+/*
+int[,] matrix = new int[5, 5];
+
+void FillMatriх(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = new Random().Next(1, 10);
+}
+}
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+System.Console.Write($" {matrix[i, j]} \t");
+}
+System.Console.WriteLine();
+}
+}
+
+void FindEvenIndex(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i += 2)
+{
+for (int j = 0; j < matrix.GetLength(1); j += 2)
+{
+matrix[i, j] = matrix[i, j] * matrix[i, j];
+}
+}
+}
+
+FillMatriх(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+FindEvenIndex(matrix);
+PrintMatrix(matrix);
+*/
 
